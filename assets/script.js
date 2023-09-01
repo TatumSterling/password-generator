92790b423bf0b86da4ad1744b6341526c80c6119
Tatum Sterling <tatum.sterling29@gmail.com>
2023-09-01 13:15:10 -0500

    added arrays of possible password characters to js file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 // Assignment Code
+//array of lowercase letters to be included in password
+var lowercase = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// array of uppercase letters to be included in password
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// array of numbers to be included in password
+var numbers = ['1','2','3', '4', '5', '6', '7', '8', '9', '0'];
+// array of special characters to be included in the password
+var special = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '{', '|', '}', '~'];
+
+var lowercasePrompt = function() {
+  
+}
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 

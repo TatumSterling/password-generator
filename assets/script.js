f2f5af891bbdf73c514bd2fa77d809a7ad4f5290
Tatum Sterling <tatum.sterling29@gmail.com>
2023-09-04 00:56:38 -0500

    added passwordlength function, which characters function, and all together function

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,55 @@
 // Assignment Code
 //array of lowercase letters to be included in password
-var lowercase = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowercaseArray = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // array of uppercase letters to be included in password
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 // array of numbers to be included in password
-var numbers = ['1','2','3', '4', '5', '6', '7', '8', '9', '0'];
+var numbersArray = ['1','2','3', '4', '5', '6', '7', '8', '9', '0'];
 // array of special characters to be included in the password
-var special = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '{', '|', '}', '~'];
+var specialArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '{', '|', '}', '~'];
 
-var lowercasePrompt = function() {
-  
+// array to add conditions to if user confirms
+var passwordinclude = [];
+//TODO figure out how to keep asking question until input is between 8 and 128
+// function to ask about the password length
+function passwordLength() {
+  var length = prompt("Please select a number of characters between 8 and 128 that you would like your password to be");
+  if (length < 8) {
+    return alert("your password must be between 8 and 128 characters.");
+  } else if (length > 128) {
+    return alert("your password must be between 8 and 128 characters.");
+  } else if (length = null) {
+    return alert("please enter a value between 8 and 128.");
+  } else {
+  }
 }
+
+//TODO make sure that you're actually combining arrays 
+// function to ask what is to be included in the password
+function whichCharacters() {
+  let lowercasePrompt = confirm("Would you like to include lowercase characters?")
+  if (lowercasePrompt) { 
+    passwordinclude.push(...lowercaseArray);}
+  let uppercasePrompt = confirm("Would you like to include uppercase letters?")
+  if (uppercasePrompt) { 
+    passwordinclude.push(...uppercaseArray);}
+  let numbersPrompt = confirm("Would you like to include numbers?")
+  if (numbersPrompt) { 
+    passwordinclude.push(...numbersArray);}
+  let specialArray = confirm("Would you like to include special characters?")
+  if (specialArray) { 
+    passwordinclude.push(...specialArray);}
+  }
+
+// function to invoke other functions in order
+function allTogetherNow() {
+  passwordLength();
+  whichCharacters();
+}
+
+allTogetherNow();
+
+console.log(passwordinclude);
 //WHEN I click the button to generate a password
 //THEN I am presented with a series of prompts for password criteria
 var generateBtn = document.querySelector("#generate");
@@ -26,4 +65,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);{
+
+}

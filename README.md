2249bd16f8b31f98c061f5144ef2e2c8837812a4
Tatum Sterling <tatum.sterling29@gmail.com>
2023-09-09 22:14:00 -0500

    added README file contents, added change to CSS, and fixed bug

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # password-generator
+
+## Description
+
+This is my password generator.  To build this application I used simple javascript to ask the user for their input, and based on their answers produce a password that meets their requirements. If you require a strong password, my generator can provide what you need!
+
+## Installation 
+
+N/A
+
+## Usage
+
+To deploy this webpage, click on the link at the top of the README file and it will take you to my password generator. From there you can click the generate password button and you will be asked a series of questions about the contents of your password. After the prompts, your password will generate into the text area where you can highlight and copy!
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo. 
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 //array of lowercase letters to be included in password
-var lowercaseArray = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // array of uppercase letters to be included in password
 var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 // array of numbers to be included in password
-var numbersArray = ['1','2','3', '4', '5', '6', '7', '8', '9', '0'];
+var numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 // array of special characters to be included in the password
 var specialArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '{', '|', '}', '~'];
 
@@ -16,48 +16,49 @@ var uppercaseRandom = "";
 var numbersRandom = "";
 var specialRandom = "";
 var passwordRandom = [];
-var userPassword= "";
+var userPassword = "";
 
 //TODO need to add if statement to check if input is in range
 // function to ask about the password length
-function lengthPrompt() {
-  do {
-    length = parseInt(prompt("How many characters would you like your password to be?"));
-  } while (length < 8 || length > 128);
-  return length;
-}
+
+  function lengthPrompt() {
+    do {
+      length = parseInt(prompt("Please choose a password length between 8 and 128 characters."));
+    } while (length < 8 || length > 128);
+    
+  }
 
 
 // function to ask what is to be included in the password, and chooses one character to be randomized
 function whichCharacters() {
   let lowercasePrompt = confirm("Would you like to include lowercase characters?")
-    if (lowercasePrompt) { 
-      passwordinclude = passwordinclude.concat(lowercaseArray);
-      lowercaseRandom = lowercaseArray[Math.floor(Math.random()*lowercaseArray.length)];
-      
-    }
+  if (lowercasePrompt) {
+    passwordinclude = passwordinclude.concat(lowercaseArray);
+    lowercaseRandom = lowercaseArray[Math.floor(Math.random() * lowercaseArray.length)];
+
+  }
   let uppercasePrompt = confirm("Would you like to include uppercase letters?")
-    if (uppercasePrompt) {  
-      passwordinclude = passwordinclude.concat(uppercaseArray);
-      uppercaseRandom = uppercaseArray[Math.floor(Math.random()*uppercaseArray.length)];
-    }
+  if (uppercasePrompt) {
+    passwordinclude = passwordinclude.concat(uppercaseArray);
+    uppercaseRandom = uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)];
+  }
   let numbersPrompt = confirm("Would you like to include numbers?")
-    if (numbersPrompt) {
-      passwordinclude = passwordinclude.concat(numbersArray);
-      numbersRandom = numbersArray[Math.floor(Math.random()*numbersArray.length)];
-    }
+  if (numbersPrompt) {
+    passwordinclude = passwordinclude.concat(numbersArray);
+    numbersRandom = numbersArray[Math.floor(Math.random() * numbersArray.length)];
+  }
   let specialPrompt = confirm("Would you like to include special characters?")
-    if (specialPrompt) { 
-      passwordinclude = passwordinclude.concat(specialArray);
-      specialRandom = specialArray[Math.floor(Math.random()*specialArray.length)];
-      console.log(passwordinclude);
-    }
+  if (specialPrompt) {
+    passwordinclude = passwordinclude.concat(specialArray);
+    specialRandom = specialArray[Math.floor(Math.random() * specialArray.length)];
+    console.log(passwordinclude);
+  }
 }
 // loop that selects random characters out of passwordinlude array
 // adds those characters to array of random characters form whichCharacters function to ensure all criteria is met
 function generatePassword() {
-  for (var i = 0; i < length-4; i++) {
-    passwordRandom[i] = passwordinclude[Math.floor(Math.random()*passwordinclude.length)];
+  for (var i = 0; i < length - 4; i++) {
+    passwordRandom[i] = passwordinclude[Math.floor(Math.random() * passwordinclude.length)];
   }
   console.log(passwordRandom);
   userPassword = passwordRandom.concat(...specialRandom, ...numbersRandom, ...uppercaseRandom, ...lowercaseRandom);
@@ -79,8 +80,8 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
- allTogetherNow();
- 
+  allTogetherNow();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = userPassword;
@@ -89,6 +90,6 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
+generateBtn.addEventListener("click", writePassword); {
 
 }
